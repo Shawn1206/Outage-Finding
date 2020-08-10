7905a12885b2e9717fedba0825c249e066e254d8
Shawn1206 <40552285+Shawn1206@users.noreply.github.com>
2020-08-10 10:55:53 -0400

    Update info_extraction.py

--- nlp project/info_extraction.py
+++ nlp project/info_extraction.py
@@ -1,5 +1,19 @@
-def function1(a):
-    pass
+import en_core_web_sm
+import spacy
+from pprint import pprint
+
+def text_cleaner(raw_text):
+    clean_t = ''
+
+    return clean_t
+
+def token_ex(clean_text):
+    nlp = en_core_web_sm.load()
+    doc = nlp(clean_text)
+    output = []
+    for X in doc.ents:
+        output.append((X.text, X.label_))
+    return output
 
 
 
