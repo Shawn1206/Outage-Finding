59aefb51a1a39ddcfe8f6f5912c63bae87d95817
Shawn1206 <40552285+Shawn1206@users.noreply.github.com>
2020-08-17 09:28:23 -0400

    Update info_extraction.py

--- nlp project/info_extraction.py
+++ nlp project/info_extraction.py
@@ -1,11 +1,13 @@
 import en_core_web_sm
 import spacy
 from pprint import pprint
+import numpy as np
 
-def text_cleaner(raw_text):
+
+def preprocess(raw_text):
     clean_t = ''
-
     return clean_t
+
 
 def token_ex(clean_text):
     nlp = en_core_web_sm.load()
@@ -15,7 +17,10 @@ def token_ex(clean_text):
         output.append((X.text, X.label_))
     return output
 
+def twitter_sp(name):
+    xfi = open(name, 'r')
+    text = xfi.read()
+    text = text.split('\n')[:-1]
+    print(text)
 
-
-
-
+twitter_sp('Xfinity.txt')
