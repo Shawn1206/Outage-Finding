2c131704bf495c69451a0dad7af0bc9e53c5899a
Shawn1206 <40552285+Shawn1206@users.noreply.github.com>
2020-09-25 10:57:21 -0400

    Update clustering.py

--- nlp project/clustering.py
+++ nlp project/clustering.py
@@ -1,24 +1,43 @@
+import csv
+import datetime
 import json
+import pickle
+import re
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+from collections import defaultdict
+
+import googlemaps
+import matplotlib.pyplot as plt
+import numpy as np
 from haversine import haversine
 from sklearn.cluster import DBSCAN
-import numpy as np
-import matplotlib.pyplot as plt
-# import cartopy
-from collections import defaultdict
-import csv
-import re
-import time
-import datetime
-import pickle
+
+from credentials import google_key
 
 
 # lyon = (45.7597, 4.8422)
 # paris = (48.8567, 2.3508)
 #
 # output = haversine(lyon, paris)
+def G_helper(name):
+    """
+    this function is utilizing Google map API for getting coordinates of certain city
+    :param name: city name, str
+    :return: coordinates, tuple
+    """
+    gmaps = googlemaps.Client(key=google_key)
+
+    geocode_result = gmaps.geocode(name)
+    return geocode_result
 
 
-def geomatch(data):
+def geo_match(data):
+    """
+    this function is for matching the input city dict with their coordinates using the data from world-city datatable
+    :param data: input dict of city names
+    :return: dict of coordinates
+    """
     output = []
     new_data = {}
     with open("city_loca.json", 'r') as f:
@@ -39,7 +58,14 @@ def geomatch(data):
 
 
 def clustering_distance(distance, data, min_p):
-    new_data = geomatch(data)
+    """
+    This is the old clustering function just for reference please ignore it
+    :param distance:
+    :param data:
+    :param min_p:
+    :return:
+    """
+    new_data = geo_match(data)
     X = np.array(new_data)
     clustering = DBSCAN(eps=distance, min_samples=min_p, metric=haversine).fit(X)
     labels = clustering.labels_
@@ -70,12 +96,9 @@ def clustering_distance(distance, data, min_p):
     plt.show()
 
 
-
-
-
-def clustering_time(name_type, time_slot, gran):
+def time_series(name_type, time_slot, gran):
     '''
-
+    This is time_series generating function for getting statistics of raw data
     :param name_type: [('Comcast.txt','t')]
     :param time_slot: ('2020-08-01','2020-09-01')
     :param gran: 'week','day','month'
@@ -156,8 +179,58 @@ def clustering_time(name_type, time_slot, gran):
     plt.savefig(name_type[0][0][:-4] + ' ' + time_slot[0] + '.pdf', bbox_inches='tight')
 
 
-lst = [('AT&T_outage AT&T.txt', 't'), ('Spectrum_outage Spectrum.txt', 't'), ('Cox.txt', 't'),
-       ('Comcast_outage Xfinity.txt', 't'), ('Verizon_outage Verizon.txt', 't')]
+def time_location_clustering(isp_name, time_slot, time_gap, distance):
+    '''
+    This function returns clustered groups
+    :param isp_name:
+    :param time_slot:
+    :param time_gap:
+    :param distance:
+    :return:
+    '''
 
-clustering_time(lst, ('2019-01-01', '2020-08-30'), 'month')
-# clustering_time(lst, ('2020-04-01', '2020-04-30'), 'day')
+    def data_preprocess(data):
+        data_formal = {}
+        for i in data:
+            time = datetime.datetime.strptime(i[:19], "%Y-%m-%d %H:%M:%S")
+            # for data[i]
+            pass
+
+    def data_slice(data, start, end):
+        output = {}
+        for i in data:
+            if start <= i <= end:
+                output[i] = data[i]
+            else:
+                continue
+        return output
+
+    def clutering_time(data, time_gap):
+        X = np.array(data)
+        clustering = DBSCAN(eps=time_gap * 3600, min_samples=1).fit(X)
+        labels = clustering.labels_
+        pass
+
+    def clustering_geo(data, distance):
+        pass
+
+    s, e = datetime.datetime.strptime(time_slot[0], "%Y-%m-%d"), datetime.datetime.strptime(
+        time_slot[1], "%Y-%m-%d")
+    f = open(isp_name)
+    data = json.load(f)
+    data_formal = data_preprocess(data)
+    data_slice1 = data_slice(data_formal, s, e)
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(111, projection=ccrs.PlateCarree())
+    ax.coastlines()
+    ax.set_extent([-130, -60, 20, 55])
+    ax.add_feature(cfeature.BORDERS.with_scale('50m'), linestyle=':')
+    ax.add_feature(cfeature.STATES.with_scale('50m'), linestyle=':')
+    tmp = data_slice1.values()
+    x, y = [i[0] for i in tmp], [i[1] for i in tmp]
+    ax.plot(x, y)
+    plt.show()
+
+# ISP_lst = [('AT&T_outage AT&T.txt', 't'), ('Spectrum_outage Spectrum.txt', 't'), ('Cox.txt', 't'),
+#        ('Comcast_outage Xfinity.txt', 't'), ('Verizon_outage Verizon.txt', 't')]
+# time_series(ISP_lst, ('2019-01-01', '2020-08-30'), 'month')
