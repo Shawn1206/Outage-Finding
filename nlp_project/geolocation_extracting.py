bc77f55a6064f13e9568e112e37c625b1b688b82
Shawn1206 <40552285+Shawn1206@users.noreply.github.com>
2020-10-23 10:45:33 -0400

    Update geolocation_extracting.py

--- nlp_project/geolocation_extracting.py
+++ nlp_project/geolocation_extracting.py
@@ -8,19 +8,47 @@ import matplotlib.pyplot as plt
 import credentials
 import twitter
 import json
+from uszipcode import SearchEngine
 
 
 def token_ex(clean_text):
+    def markercleaner(string):
+        pass
+    def casehelper(string):
+        new = string.split(' ')
+        output = ''
+        for i in range(len(new)):
+            if new[i] and new[i] != ' ':
+                new[i] = new[i][0].upper() + new[i][1:]
+            output += new[i]
+            if i != len(new) - 1:
+                output += ' '
+        return output
+    tokens = clean_text.split(' ')
+    clean_text = ''
+    for token in tokens:
+        new_token = ''.join(e for e in token if e.isalnum())
+        clean_text += new_token + ' '
     nlp = en_core_web_sm.load()
     doc = nlp(clean_text)
     output = []
     for X in doc.ents:
         if X.label_ == 'GPE':
             output.append((X.text, X.label_))
+    if not output:
+        words = clean_text.split(' ')
+        for word in words:
+            word = ''.join(e for e in word if e.isalnum())
+            if word.isdigit() and len(word) == 5:
+                search = SearchEngine(simple_zipcode=True)
+                zipcode = search.by_zipcode(word)
+                res = zipcode.to_dict()
+                output.append((res['major_city'], 'GPE'))
+
     return output
 
 
-print(token_ex('An outage '))
+print(token_ex('1237270689087803392 2020-03-10 02:54:54 EDT <amyl_olsen> @Ask_Spectrum is there an outage in #NY?'))
 
 def twitter_sp(name):
     def twitter_profile(user):
@@ -42,10 +70,11 @@ def twitter_sp(name):
     tmp = open(name, 'r')
     text = tmp.read()
     text = text.split('\n')[:-1]
+    # print(len(set(text)))
     output = defaultdict(list)
     p1 = re.compile(r'[<](.*?)[>]')
     for tweet in text:
-        username = re.findall(p1, tweet)[0]
+        # username = re.findall(p1, tweet)[0]
         geo_stat = ''
         # geo_stat = twitter_profile(username)
         time = tweet[20:44]
@@ -58,7 +87,8 @@ def twitter_sp(name):
     with open('loca_' + name + '.json', 'w') as outfile:
         json.dump(output, outfile)
 
-# twitter_sp('Cox.txt')
+
+# twitter_sp('Comcast.txt')
 
 
 def reddit_sp(name):
@@ -102,53 +132,45 @@ def forum_sp(name):
 
 # forum_sp('Xfinity_forum_data0.1.txt')
 
-def location_filter(dict0):
-    empty = []
+# def location_filter(dict0):
+#     empty = []
+#
 
-    def casehelper(string):
-        new = string.split(' ')
-        output = ''
-        for i in range(len(new)):
-            if new[i] and new[i] != ' ':
-                new[i] = new[i][0].upper() + new[i][1:]
-            output += new[i]
-            if i != len(new) - 1:
-                output += ' '
-        return output
 
-    gc = geonamescache.GeonamesCache()
-    county = {}
-    for coun in gc.get_us_counties():
-        county[coun['name']] = coun
-    for time in dict0.keys():
-        tmp = []
-        for post in dict0[time]:
-            tmp2 = []
-            for tuple in post:
-                # if not tuple[0][0].isupper():
-                #     tmp3 = casehelper(tuple[0])
-                # else:
-                #     tmp3 = tuple[0]
-                tmp3 = casehelper(tuple[0])
-                if tmp3 not in county and tmp3 not in gc.get_us_states_by_names() and \
-                        tmp3 not in gc.get_us_states() and gc.get_cities_by_name(tmp3) == []:
-                    print(tmp3)
-                    continue
-                else:
-                    tmp2.append(tmp3)
-            tmp.append(tmp2)
-        if tmp[0]:
-            dict0[time] = tmp
-        else:
-            empty.append(time)
-    for key in empty:
-        dict0.pop(key)
-    return dict0
+
+#
+#     gc = geonamescache.GeonamesCache()
+#     county = {}
+#     for coun in gc.get_us_counties():
+#         county[coun['name']] = coun
+#     for time in dict0.keys():
+#         tmp = []
+#         for post in dict0[time]:
+#             tmp2 = []
+#             for tuple in post:
+#                 # if not tuple[0][0].isupper():
+#                 #     tmp3 = casehelper(tuple[0])
+#                 # else:
+#                 #     tmp3 = tuple[0]
+#                 tmp3 = casehelper(tuple[0])
+#                 if tmp3 not in county and tmp3 not in gc.get_us_states_by_names() and \
+#                         tmp3 not in gc.get_us_states() and gc.get_cities_by_name(tmp3) == []:
+#                     print(tmp3)
+#                     continue
+#                 else:
+#                     tmp2.append(tmp3)
+#             tmp.append(tmp2)
+#         if tmp[0]:
+#             dict0[time] = tmp
+#         else:
+#             empty.append(time)
+#     for key in empty:
+#         dict0.pop(key)
+#     return dict0
 
 
 def name_to_number(name):
     pass
-
 
 # gc = geonamescache.GeonamesCache()
 # raw = gc.get_cities()
@@ -158,5 +180,6 @@ def name_to_number(name):
 #         raw2.pop(i)
 #
 # print(len(raw2))
+
 # print(len(dict0))
 # print(location_filter(dict0))
