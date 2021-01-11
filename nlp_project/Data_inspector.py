dda28e1649a05d637e957904fa8830f8bb72de16
Shawn1206 <40552285+Shawn1206@users.noreply.github.com>
2021-01-11 10:30:04 -0500

    Update Data_inspector.py

--- nlp_project/Data_inspector.py
+++ nlp_project/Data_inspector.py
@@ -1,30 +1,40 @@
+# This file is for evaluating how effective the NLP algorithm is when tagging the location name by checking the
+# precision and recall of a randomly generated sample. I wrote this program to make manually checking data easier on
+# the terminal and you can manipulate it at will to fulfill your need.
 import random
 import csv
 import sys
 import en_core_web_sm
 from geolocation_extracting import token_ex
-num_t = 200
-text = []
-file_name = sys.argv[1]
+
+num_t = 100  # the size of sample
+text = []  # used for holding the input data
+file_name = sys.argv[1]  # terminal input of dataset's filename
+
+# the input is set to be .tsv file and you can change this part at will
 with open(file_name, newline='') as tsvfile:
     reader = csv.reader(tsvfile, delimiter='\t')
     for row in reader:
         text.append(row[-1])
 text = text[1:]
+
 num = len(text)
 list_sample = random.sample(range(num), num_t)
+# The following five lists are used to hold the indexes of corresponding entries in the dataset
 total_num = []
 reco_num = []
 unreco = []
 misreco = []
 weird = []
+
 c_num = num_t
-with open('log_for_' + file_name[-10::-4] + '.txt', 'a') as file:
+with open('log_for_' + file_name[-10::-4] + '.txt', 'a') as file:  # you can custom the output file name
     for i in list_sample:
         print('====================================' + '\n' + '====================================')
         print(str(c_num) + ' / ' + str(num_t))
         c_num -= 1
         flag = 0
+        # This part basically depends on your NLP algorithm's API
         nlp = en_core_web_sm.load()
         doc = nlp(text[i])
         for X in doc.ents:
@@ -32,6 +42,8 @@ with open('log_for_' + file_name[-10::-4] + '.txt', 'a') as file:
                 reco_num.append(i)
                 flag = 1
                 break
+
+        # printing the data entry on the terminal and collecting feedback
         print(text[i] + '\n')
         feedback = input("Contains location words ([Y]/N)?")
         feedback2 = input("Is this tweet weird ([Y]/N)?")
@@ -56,18 +68,21 @@ with open('log_for_' + file_name[-10::-4] + '.txt', 'a') as file:
     c = set(unreco)
     d = set(misreco)
     e = set(weird)
-    a -= e
+    a -= e  # get rid of entries which are mistakenly put into our dataset
     b -= e
     c -= e
     d -= e
     count = 0
+    # sometimes certain entries need to be reexamined because some unknown reason makes the NLP algorithm failed to 
+    # process them in the first run 
     f = []
     for i in c:
         output = token_ex(text[i])
         if output:
             count += 1
             f.append(i)
-    recall = (len(a) - len(c) + count) / len(a)
+            
+    recall = (len(a) - len(c) + count) / len(a)  # calculate the results
     precision = (len(b) - len(d) + count) / (len(b) + count)
     b |= set(f)
     c -= set(f)
@@ -82,5 +97,5 @@ with open('log_for_' + file_name[-10::-4] + '.txt', 'a') as file:
     file.write("The list of total tweets contains locations not tagged by machine: " + str(c) + '\n')
     file.write("The list of total tweets contains locations mistakenly tagged by machine: " + str(misreco) + '\n')
     file.write("The list of weird tweets in the sample: " + str(weird) + '\n')
-    file.write("Precision is: "+str(precision))
-    file.write("Recall is "+str(recall))
+    file.write("Precision is: " + str(precision))
+    file.write("Recall is " + str(recall))
